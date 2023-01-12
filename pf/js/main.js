9c41bef080f8bb28c97b0f416e369645c2646285
hakim4web <122433161+hakim4web@users.noreply.github.com>
2023-01-12 08:35:18 +0100

    Add files via upload

---
+++ pf/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    setTimeout(() => {
+        $('.preloader').fadeOut('slow')
+    }, 2100);
+
+    $('.home .talk').click(function(){
+        $('.layer').fadeIn('slow')
+    })
+
+    $('.close').click(function(){
+        $('.layer').fadeOut('slow')
+    })
+
+    $('.sendmsg').click(function(){
+        check();
+    })
+})
+
+
+function check() {
+    switch (0) {
+        case $('.email').val().length:
+            alert('Enter your email please');
+            break;
+
+        case $('.message').val().length:
+            alert('Say something ;)');
+            break;
+
+        default:
+            send();
+            break;
+    }
+}
+
+function send() {
+
+    $('.mini-loader').fadeIn(0)
+    
+    var xh = new XMLHttpRequest();
+    var msg = document.getElementsByClassName('message')[0].value;
+    var email = document.getElementsByClassName('email')[0].value;
+    var data = 'email=' + email + '&msg=' + msg;
+
+    xh.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var rsp = this.responseText;
+            $('.mini-loader').fadeOut(0,function(){
+                document.getElementsByClassName('res')[0].innerHTML = rsp
+            })
+        }
+    }
+    xh.open("POST", "http://localhost/mail/index.php", true);
+    xh.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xh.send(data);
+}
